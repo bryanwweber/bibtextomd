502f19f8fbed533f92500716c00dd855e1c4ca3b
Bryan W. Weber <bryan@bryanwweber.com>
2016-04-30 17:42:45 -0400

    Bump version to 0.4.0

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2014 Bryan W. Weber
+Copyright (c) 2016 Bryan W. Weber
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.0',
+    version='0.4.0',
 
     description='Convert BibTeX to Markdown',
     long_description=long_description,
@@ -41,7 +41,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
