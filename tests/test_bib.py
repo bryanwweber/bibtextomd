e233279e59c1e11a00092be73b5aa194ce1514f8
Bryan W. Weber <bryan@bryanwweber.com>
2016-04-30 12:46:21 -0400

    Test the journal_article function

--- tests/test_bib.py
+++ tests/test_bib.py
@@ -2,7 +2,7 @@
 Testing module for bib.py
 """
 import pytest
-from bibtextomd.bib import main, reorder
+from bibtextomd.bib import main, reorder, load_bibtex, journal_article
 
 
 def test_single_author_good():
@@ -46,3 +46,22 @@ def test_hyphenated_name():
     n = reorder(names, None)
     assert n == 'H.A. Name'
 
+
+@pytest.fixture
+def load_bibtex_for_test():
+    return load_bibtex('tests/refs.bib')
+
+
+def test_journal_article(load_bibtex_for_test):
+    sort_dict = load_bibtex_for_test
+    ref = sort_dict["article"][0]
+    reference = journal_article(ref, None)
+    reference_blessed = (
+        "\n{:.paper}\n"
+        "<span>A study of the best ways to make up a name</span>{:.papertitle}"
+        "  \n<span>F.A. Author, S.B. Sécond, and T.C. Third</span>{:.authors}"
+        "  \n<span>_Journal Of Made Up Names_, Aug. 2013</span>{:.journal}"
+        "  \n<span>**DOI:** [10.0000/made-up-doi](http://dx.doi.org/10.0000/made-up-doi)</span>"
+        "{:.doi}  \n"
+        )
+    assert reference == reference_blessed
